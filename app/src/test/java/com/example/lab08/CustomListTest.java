2ba2efefcc6967bfa6bb89dd6f627326a0261fb6
Mohamed Irfan <mmohame6@ualberta.ca>
2025-10-28 17:57:38 -0600

    countCities fail

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -25,4 +25,18 @@ public class CustomListTest {
         assertFalse(list.hasCity(Calgary));
     }
 
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City Calgary = new City("Calgary", "AB");
+        list.addCity(Calgary);
+
+        assertEquals(1, list.countCities());
+
+        City Edm = new City("Edmonton", "AB");
+        list.addCity(Edm);
+
+        assertEquals(2, list.countCities());
+
+    }
 }
